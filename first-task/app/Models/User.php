cdede469ddc391fe60a5437bbc763b54b06f2de3
Fresh Dev <billyhans90@gmail.com>
2024-11-18 10:56:53 +0100

    Feature [Models&Migrations]: Setup models and migrations needed

--- first-task/app/Models/User.php
+++ first-task/app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -44,5 +46,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function avatar(): MorphOne
+    {
+        return $this->morphOne(EntityImage::class, 'imageable')->latestOfMany();
+    }
+
+    public function talent(): HasOne
+    {
+        return $this->hasOne(Talent::class);
     }
 }
---
+++ first-task/database/migrations/2024_11_18_095422_create_jobs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('talent_id')->constrained()->cascadeOnDelete();
+            $table->string('position');
+            $table->string('company');
+            $table->enum('status', ['applied', 'interview', 'offer', 'rejected'])->default('applied');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jobs');
+    }
+};
